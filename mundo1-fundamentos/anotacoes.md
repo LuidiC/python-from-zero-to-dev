b14e16ae5b265b6bbf6189da92fd6bf7be190343
LuidiC <cadeteluidi817@gmail.com>
2026-04-20 16:53:28 -0300

    aula04

---
+++ mundo1-fundamentos/anotacoes.md
@@ -0,0 +1,9 @@
+# Anotações Gerais
+
+. Aula04
+
+- No python para delimitar as mensagens de dois modos, com aspas simples '' ou aspas duplas "", mas a grande maioria dos programadores utilizam as aspas simples.
+  Ex: print('Hello World')
+
+- Comando para concatenar textos: print('exemplo1'+'exemplo2') = exemplo1exemplo2
+  Podemos fazer isso com o "+"(Quando tiver apenas textos) ou com a ","(Quando tiver números e textos por exemplo, diferentes tipos de variáveis), existem casos que um é melhor e que outro é melhor.
---
+++ mundo1-fundamentos/desafios/aula04.py
@@ -0,0 +1,21 @@
+# Desafio 01
+print('=== DESAFIO 01 ===')
+nome = input('Qual o seu nome? ')
+
+print(f'Olá {nome}! Seja bem-vindo!')
+
+# Desafio 02
+print('=== DESAFIO 02 ===')
+dia = input('Qual o dia que você nasceu? ')
+mes = input('Qual o mês que você nasceu? ')
+ano = input('Qual o ano que você nasceu? ')
+
+print(f'Você nasceu no dia {dia} de {mes} de {ano}.')
+
+# Desafio 03
+print('=== DESAFIO 03 ===')
+num1 = int(input('Digite o primeiro número: '))
+num2 = int(input('Digite o segundo número: '))
+soma = num1 + num2
+
+print(f'A soma entre esses números é {soma}')
