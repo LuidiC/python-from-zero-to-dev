caa678754b33c8b2ef97076b73f7e49319cf99ca
LuidiC <130425520+LuidiC@users.noreply.github.com>
2026-06-17 17:25:46 -0300

    update

--- mundo1-fundamentos/desafios/ex027.py
+++ mundo1-fundamentos/desafios/ex027.py
@@ -9,7 +9,6 @@ print('=' * 5  + ' DESAFIO 27 ' + '=' * 5)
 
 name = input('Digite seu nome completo: ')
 lista_name = name.split(' ')
-print(lista_name)
 first_name = lista_name[0]
 last_name = lista_name[len(lista_name)-1] 
 print(f'Primeiro nome: {first_name}')
